84a571464637dc033808b576719d509ae64c58a5
Sahaj Singh <Sahaj@vt.edu>
2024-05-09 17:41:46 -0400

    Update AboutUs.js
    hello vro taha wrote this

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -4,7 +4,13 @@ const AboutUs = () => {
   return (
     <div>
       <h1>About Us</h1>
-      <p>This is the About Us page.</p>
+      <p>We've all had plants in our homes. For those who haven't experienced this sensation, if you are a beginner,
+        majority of the time your plant will die. This is because you either drowned your poor plant in water, or made the plant think
+        it was facing a drought due to the lack of water. Nonetheless, the level of moisture is not up to the standards of the plant.
+        This is where we got our idea from, recently we've all had a plant die on us, and we became saddened by these unforeseen 
+        circumstances. So we came up with a solution for our problem. By utilizing aurdino, sensors, and front end development tools, 
+        we have created an efficient way to detect moisture levels. We call this system the Garden Angel, and we will show you how to
+        use this.</p>
     </div>
   );
 };
